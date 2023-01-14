9bc793f87a8c2ba6f05bcf01ee431626bbfeb351
Robin Six <55784799+sixrobin@users.noreply.github.com>
2023-01-14 21:41:18 +0100

    CRT data not global anymore.

--- Assets/Scripts/CameraCRT.cs
+++ Assets/Scripts/CameraCRT.cs
@@ -4,19 +4,61 @@ using UnityEngine;
 [RequireComponent(typeof(Camera))]
 public class CameraCRT : MonoBehaviour
 {
+    [Header("SHADER")]
     [SerializeField] private Shader _shader = null;
 
+    [Header("DATA")]
+    [SerializeField, Range(1f, 30f)] private float _curvature = 5f;
+    [SerializeField, Range(0f, 100f)] private float _vignetteWidth = 100f;
+    [SerializeField, Range(0.5f, 3f)] private float _scanlinesMultiplier = 1f;
+    [SerializeField] private Vector3 _rgbMultiplier = new(0.25f, 0.2f, 0.3f);
+    
+    private static readonly int s_crtCurvature = Shader.PropertyToID("_Curvature");
+    private static readonly int s_crtVignetteWidth = Shader.PropertyToID("_VignetteWidth");
+    private static readonly int s_crtScanlinesMultiplier = Shader.PropertyToID("_ScanlinesMultiplier");
+    private static readonly int s_crtRGBMultiplier = Shader.PropertyToID("_RGBMultiplier");
+    
     private Material _material;
 
-    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    private bool InitMaterial()
     {
         if (_shader == null)
-            return;
+            return false;
 
         if (_material == null)
             _material = new Material(_shader);
 
+        return true;
+    }
+    
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (!this.InitMaterial())
+            return;
+
+        this.UpdateValues();
         Graphics.Blit(source, destination, _material);
+    }
+
+    private void OnValidate()
+    {
+        if (!this.InitMaterial())
+            return;
+        
+        this.UpdateValues();
+    }
+
+    private void UpdateValues()
+    {
+        _material.SetFloat(s_crtCurvature, this._curvature);
+        _material.SetFloat(s_crtVignetteWidth, this._vignetteWidth);
+        _material.SetFloat(s_crtScanlinesMultiplier, this._scanlinesMultiplier);
+        _material.SetVector(s_crtRGBMultiplier, this._rgbMultiplier); 
+    }
+
+    private void OnEnable()
+    {
+        this.InitMaterial();
     }
 
     private void OnDisable()
--- Assets/Scripts/GlobalShaderVariables.cs
+++ Assets/Scripts/GlobalShaderVariables.cs
@@ -8,19 +8,9 @@ public class GlobalShaderVariables : ScriptableObject
     [SerializeField, Range(0.3f, 0.45f)] private float _depthSmoothCenter = 0.4f;
     [SerializeField, Range(0f, 0.01f)] private float _depthSmoothValue = 0.006f;
     
-    [Header("CRT")]
-    [SerializeField, Range(1f, 30f)] private float _CRTCurvature = 5f;
-    [SerializeField, Range(0f, 100f)] private float _CRTVignetteWidth = 100f;
-    [SerializeField, Range(0.5f, 3f)] private float _CRTScanlinesMultiplier = 1f;
-    [SerializeField] private Vector3 _CRTRBGMultiplier = new Vector3(0.25f, 0.2f, 0.3f);
-    
     private static readonly int s_depthMultiplier = Shader.PropertyToID("_DepthMultiplier");
     private static readonly int s_depthSmoothCenter = Shader.PropertyToID("_DepthSmoothCenter");
     private static readonly int s_depthSmoothValue = Shader.PropertyToID("_DepthSmoothValue");
-    private static readonly int s_crtCurvature = Shader.PropertyToID("_Curvature");
-    private static readonly int s_crtVignetteWidth = Shader.PropertyToID("_VignetteWidth");
-    private static readonly int s_crtScanlinesMultiplier = Shader.PropertyToID("_ScanlinesMultiplier");
-    private static readonly int s_crtRGBMultiplier = Shader.PropertyToID("_RGBMultiplier");
 
     private static System.Collections.Generic.List<T> FindAssetsByType<T>() where T : UnityEngine.Object
     {
@@ -65,10 +55,6 @@ public class GlobalShaderVariables : ScriptableObject
         Shader.SetGlobalFloat(s_depthMultiplier, this._depthMultiplier);
         Shader.SetGlobalFloat(s_depthSmoothCenter, this._depthSmoothCenter);
         Shader.SetGlobalFloat(s_depthSmoothValue, this._depthSmoothValue);
-        Shader.SetGlobalFloat(s_crtCurvature, this._CRTCurvature);
-        Shader.SetGlobalFloat(s_crtVignetteWidth, this._CRTVignetteWidth);
-        Shader.SetGlobalFloat(s_crtScanlinesMultiplier, this._CRTScanlinesMultiplier);
-        Shader.SetGlobalVector(s_crtRGBMultiplier, this._CRTRBGMultiplier);
     }
 
     private void OnValidate()
