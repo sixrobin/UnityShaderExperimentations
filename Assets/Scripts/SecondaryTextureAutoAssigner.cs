fb3c1f9d725a44486fb16abe8e2dd4410f92c84e
Robin Six <55784799+sixrobin@users.noreply.github.com>
2023-02-11 16:49:18 +0100

    Update SecondaryTextureAutoAssigner.cs

--- Assets/Scripts/SecondaryTextureAutoAssigner.cs
+++ Assets/Scripts/SecondaryTextureAutoAssigner.cs
@@ -1,8 +1,8 @@
-using UnityEngine;
-using UnityEditor;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
 
 public static class SecondaryTextureAutoAssigner
 {
@@ -42,79 +42,76 @@ public static class SecondaryTextureAutoAssigner
     [MenuItem("Tools/Secondary Textures Assigner/Auto assign for selected folder")]
     private static void AutoAssignForSelectedFolder()
     {
-        Object selectedObject = Selection.activeObject;
-
-        if (selectedObject == null)
-        {
-            Debug.LogWarning("Selection is null!");
+        if (!TryGetValidSelection(out string path))
             return;
-        }
-        
-        string path = AssetDatabase.GetAssetPath(selectedObject.GetInstanceID());
 
-        if (path.Length <= 0)
-        {
-            Debug.LogWarning("Selection is not in Assets folder!");
-            return;
-        }
-
-        if (!Directory.Exists(path))
-        {
-            Debug.LogWarning("Selection is not a folder!");
-            return;
-        }
-        
+        // Get candidate folders.
         string[] folders = GetFoldersAndSubfoldersRecursively(path);
         FilterFoldersWithoutFiles(ref folders);
 
+        // Parse folders content to gather textures and their importers.
         Dictionary<string, TextureImporterData[]> textureImporterDataPerFolder = new();
         foreach (string folder in folders)
             if (TryComputeTextureImporterDataForFolder(folder, out TextureImporterData[] textureImporterData))
                 textureImporterDataPerFolder.Add(folder, textureImporterData);
 
+        // Get together main and secondary textures.
         List<SecondaryTexturesData> secondaryTexturesData = new();
         foreach (KeyValuePair<string, TextureImporterData[]> textureImporterData in textureImporterDataPerFolder)
             secondaryTexturesData.AddRange(ComputeSecondaryTexturesDataForFolder(textureImporterData.Key, textureImporterData.Value));
 
+        // Reimport main textures with their secondary textures.
         secondaryTexturesData.ForEach(AssignSecondaryTextures);
     }
 
     [MenuItem("Tools/Secondary Textures Assigner/Cleanup for selected folder")]
     private static void CleanupForSelectedFolder()
     {
-        Object selectedObject = Selection.activeObject;
-
-        if (selectedObject == null)
-        {
-            Debug.LogWarning("Selection is null!");
+        if (!TryGetValidSelection(out string path))
             return;
-        }
         
-        string path = AssetDatabase.GetAssetPath(selectedObject.GetInstanceID());
-
-        if (path.Length <= 0)
-        {
-            Debug.LogWarning("Selection is not in Assets folder!");
-            return;
-        }
-
-        if (!Directory.Exists(path))
-        {
-            Debug.LogWarning("Selection is not a folder!");
-            return;
-        }
-        
+        // Get candidate folders.
         string[] folders = GetFoldersAndSubfoldersRecursively(path);
         FilterFoldersWithoutFiles(ref folders);
 
+        // Parse folders content to gather textures and their importers.
         Dictionary<string, TextureImporterData[]> textureImporterDataPerFolder = new();
         foreach (string folder in folders)
             if (TryComputeTextureImporterDataForFolder(folder, out TextureImporterData[] textureImporterData))
                 textureImporterDataPerFolder.Add(folder, textureImporterData);
 
+        // Cleanup secondary textures.
         foreach (KeyValuePair<string, TextureImporterData[]> textureImporterData in textureImporterDataPerFolder)
             foreach (TextureImporterData importerData in textureImporterData.Value)
                 CleanupSecondaryTextures(importerData.Importer);
+    }
+
+    private static bool TryGetValidSelection(out string selectionPath)
+    {
+        selectionPath = string.Empty;
+        Object selectedObject = Selection.activeObject;
+
+        if (selectedObject == null)
+        {
+            Debug.LogWarning("Selection is null!");
+            return false;
+        }
+        
+        selectionPath = AssetDatabase.GetAssetPath(selectedObject.GetInstanceID());
+
+        if (selectionPath.Length <= 0)
+        {
+            Debug.LogWarning("Selection is not in Assets folder!");
+            return false;
+        }
+
+        if (!Directory.Exists(selectionPath))
+        {
+            Debug.LogWarning($"Selection is not a folder (selection path: {selectionPath})!");
+            return false;
+        }
+
+        return true;
     }
     
     private static string[] GetFoldersAndSubfoldersRecursively(string path, bool includeRoot = true)
@@ -161,9 +158,9 @@ public static class SecondaryTextureAutoAssigner
     {
         List<SecondaryTexturesData> result = new();
 
-        foreach (TextureImporterData folderTexture in folderTextures)
-            if (SecondaryTexturesNames.All(o => !folderTexture.Texture.name.Contains(o)))
-                result.Add(ComputeSecondaryTexturesData(folder, folderTexture, folderTextures));
+        foreach (TextureImporterData folderTexture in folderTextures)                                // Foreach texture in the folder.
+            if (SecondaryTexturesNames.All(o => !folderTexture.Texture.name.Contains(o)))       // If texture is not a secondary texture.
+                result.Add(ComputeSecondaryTexturesData(folder, folderTexture, folderTextures)); // Get its secondary textures.
 
         return result.ToArray();
     }
@@ -176,11 +173,11 @@ public static class SecondaryTextureAutoAssigner
             SecondaryTextures = new Dictionary<string, Texture2D>()
         };
         
-        foreach (TextureImporterData folderTexture in folderTextures)
-            if (folderTexture.Texture != targetTexture.Texture)
-                foreach (string secondaryTextureName in SecondaryTexturesNames)
-                    if (targetTexture.Texture.name + secondaryTextureName == folderTexture.Texture.name)
-                        result.SecondaryTextures.Add(secondaryTextureName, folderTexture.Texture);
+        foreach (TextureImporterData folderTexture in folderTextures)                                        // Foreach texture in the folder.
+            if (folderTexture.Texture != targetTexture.Texture)                                              // Ignore target texture.
+                foreach (string secondaryTextureName in SecondaryTexturesNames)                              // Foreach secondary texture names.
+                    if ($"{targetTexture.Texture.name}{secondaryTextureName}" == folderTexture.Texture.name) // If texture's name correspond to a secondary texture.
+                        result.SecondaryTextures.Add(secondaryTextureName, folderTexture.Texture);           // Record the name/suffix and the texture.
 
         return result;
     }
