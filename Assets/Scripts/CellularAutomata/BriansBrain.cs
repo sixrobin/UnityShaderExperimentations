0c715597f093af24f3e6cab3ec7035546fa173b0
Robin Six <55784799+sixrobin@users.noreply.github.com>
2023-09-10 18:50:01 +0200

    Resolution enum

---
+++ Assets/Scripts/CellularAutomata/BriansBrain.cs
@@ -0,0 +1,23 @@
+public class BriansBrain : CellularAutomaton
+{
+    protected override void Init()
+    {
+        base.Init();
+        
+        this._computeShader.SetTexture(0, "Result", this._gridBuffer);
+        this._computeShader.Dispatch(0, this.Resolution / 8, this.Resolution / 8, 1);
+
+        this._computeShader.SetTexture(2, "Result", this._grid);
+        this._computeShader.SetTexture(2, "GridBuffer", this._gridBuffer);
+        this._computeShader.Dispatch(2, this.Resolution / 8, this.Resolution / 8, 1);
+    }
+
+    protected override void Next()
+    {
+        this._computeShader.SetTexture(1, "Result", this._grid);
+        this._computeShader.SetTexture(1, "GridBuffer", this._gridBuffer);
+        this._computeShader.Dispatch(1, this.Resolution / 8, this.Resolution / 8, 1);
+        
+        this.ApplyTextureBuffer();
+    }
+}
